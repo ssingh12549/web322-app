12fd0aa8165a9a6918c98f16b039b3c02a210d8a
ssingh12549 <132972297+ssingh12549@users.noreply.github.com>
2023-06-19 19:12:12 -0400

    Update officeData.js

--- modules/officeData.js
+++ modules/officeData.js
@@ -1,86 +1,82 @@
-const fs = require("fs");
-
-class Data{
-    constructor(employees, classes){
-        this.employees = employees;
-        this.classes = classes;
+class Data {
+    employees;
+    classes;
+  
+    constructor(employees, classes) {
+      this.employees = employees;
+      this.classes = classes;
     }
-}
-
-let dataCollection = null;
-
-module.exports.initialize = function () {
-    return new Promise( (resolve, reject) => {
-        fs.readFile('./data/classes.json','utf8', (err, classesData) => {
+  }
+  
+  let dataCollection = null;
+  
+  module.exports.initialize = () => {
+    const fs = require("fs");
+    return new Promise((resolve, reject) => {
+      fs.readFile("./data/employees.json", "utf8", (err, dataEmployees) => {
+        if (err) {
+          reject("unable to read employees.json");
+        } else {
+          fs.readFile("./data/classes.json", "utf8", (err, dataClasses) => {
             if (err) {
-                reject("unable to load classes"); return;
+              reject("unable to read classes.json");
+              return;
+            } else {
+              dataCollection = new Data(
+                JSON.parse(dataEmployees),
+                JSON.parse(dataClasses)
+              );
+              resolve();
             }
-
-            fs.readFile('./data/employees.json','utf8', (err, employeeData) => {
-                if (err) {
-                    reject("unable to load employees"); return;
-                }
-
-                dataCollection = new Data(JSON.parse(employeeData), JSON.parse(classesData));
-                resolve();
-            });
-        });
+          });
+        }
+      });
     });
-}
-
-module.exports.getEmployeeByNum = function(){
-    return new Promise((resolve,reject)=>{
-        if (dataCollection.employees.length == 0) {
-            reject("query returned 0 results"); return;
-        }
-
-        resolve(dataCollection.employees);
-    })
-}
-
-module.exports.getEAs = function () {
-    return new Promise(function (resolve, reject) {
-        var filteredemployees = [];
-
-        for (let i = 0; i < dataCollection.employees.length; i++) {
-            if (dataCollection.employees[i].EA == true) {
-                filteredemployees.push(dataCollection.employees[i]);
-            }
-        }
-
-        if (filteredemployees.length == 0) {
-            reject("query returned 0 results"); return;
-        }
-
-        resolve(filteredemployees);
+  };
+  
+  module.exports.getAllEmployees = () =>
+    new Promise((resolve, reject) => {
+      dataCollection.employees.length === 0
+        ? reject("no results returned")
+        : resolve(dataCollection.employees);
     });
-};
-
-module.exports.getPartTimers = function () {
-    return new Promise(function (resolve, reject) {
-        var filteredemployees = [];
-
-        for (let i = 0; i < dataCollection.employees.length; i++) {
-            if (dataCollection.employees[i].status == "Part Time") {
-                filteredemployees.push(dataCollection.employees[i]);
-            }
-        }
-
-        if (filteredemployees.length == 0) {
-            reject("query returned 0 results"); return;
-        }
-
-        resolve(filteredemployees);
-    });
-};
-
-module.exports.getclasses = function(){
-   return new Promise((resolve,reject)=>{
-    if (dataCollection.classes.length == 0) {
-        reject("query returned 0 results"); return;
-    }
-
-    resolve(dataCollection.classes);
-   });
-}
-
+  
+  module.exports.getEAs = () =>
+    new Promise((resolve, reject) =>
+      dataCollection.employees.filter((employee) => employee.EA).length === 0
+        ? reject("no results returned")
+        : resolve(dataCollection.employees.filter((employee) => employee.EA))
+    );
+  
+  module.exports.getClasses = () =>
+    new Promise((resolve, reject) =>
+      dataCollection.classes.length === 0
+        ? reject("no results returned")
+        : resolve(dataCollection.classes)
+    );
+  
+  module.exports.getPartTimers = () =>
+    new Promise((resolve, reject) =>
+      dataCollection.employees.filter(
+        (employee) => employee.status === "Part Time"
+      ).length === 0
+        ? reject("no results returned")
+        : resolve(
+            dataCollection.employees.filter(
+              (employee) => employee.status === "Part Time"
+            )
+          )
+    );
+  
+  module.exports.getEmployeeByNum = (num) =>
+    new Promise((resolve, reject) =>
+      dataCollection.employees.find(
+        (employee) => employee.employeeNum === num
+      ) === undefined
+        ? reject("no results returned")
+        : resolve(
+            dataCollection.employees.find(
+              (employee) => employee.employeeNum === num
+            )
+          )
+    );
